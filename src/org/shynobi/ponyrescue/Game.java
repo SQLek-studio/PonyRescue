41d44adc1fac51ea836ed297f14ed8cf3c10c5ab
Piotr SQLek Skólski <piotr.sqlek@gmail.com>
2014-05-21 19:14:15 +0200

    Render test

--- src/org/shynobi/ponyrescue/Game.java
+++ src/org/shynobi/ponyrescue/Game.java
@@ -21,8 +21,13 @@ package org.shynobi.ponyrescue;
 
 import com.jme3.app.DebugKeysAppState;
 import com.jme3.app.SimpleApplication;
+import com.jme3.app.StatsAppState;
 import com.jme3.app.state.AppState;
+import com.jme3.material.Material;
 import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
+import com.jme3.post.filters.CartoonEdgeFilter;
+import com.jme3.post.ssao.SSAOFilter;
 import com.jme3.scene.Spatial;
 
 /**
@@ -36,11 +41,11 @@ public class Game extends SimpleApplication {
     }
     
     @Override
-    public void simpleInitApp() {/*
-        Material groundMaterial = new Material(assetManager, 
+    public void simpleInitApp() {
+        Material material = new Material(assetManager, 
         "Common/MatDefs/Misc/Unshaded.j3md");
-        groundMaterial.setColor("Color", ColorRGBA.Green);
-        
+        //groundMaterial.setColor("Color", ColorRGBA.Green);
+        /*
         Material towerMaterial = new Material(assetManager, 
         "Common/MatDefs/Misc/Unshaded.j3md");
         //towerMaterial.setColor("Color", ColorRGBA.Orange);
@@ -51,16 +56,31 @@ public class Game extends SimpleApplication {
         */
         Spatial tower = assetManager.loadModel("Scenes/Main.j3o");
         //Geometry tower = new Geometry("Tower",new Box(1,4,1));
-        //tower.setMaterial(towerMaterial);
+        tower.setMaterial(material);
         rootNode.attachChild(tower);
         
         cam.setLocation(new Vector3f(36,36,36));
         cam.lookAt(Vector3f.ZERO, Vector3f.UNIT_Y);
         //flyCam.setMoveSpeed(20);
+        
+        FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
+        viewPort.addProcessor(fpp);
+        
+        CartoonEdgeFilter cartoon = new CartoonEdgeFilter();
+        //cartoon.setEdgeWidth(1.2f);
+        cartoon.setDepthSensitivity(2);
+        
+        SSAOFilter ssaoFilter = new SSAOFilter();
+        //ssaoFilter.setBias(0.3f);
+        ssaoFilter.setIntensity(5.2f);
+        ssaoFilter.setSampleRadius(1.3f);
+        
+        fpp.addFilter(ssaoFilter);
+        fpp.addFilter(cartoon);
     }
     
     public static void main(String[] args) {
-        Game game = new Game(new GsGame(), new GsMenuMain(), new GsFreeFly());
+        Game game = new Game(new GsGame(), new GsMenuMain(), new GsFreeFly(), new StatsAppState());
         game.start();
     }
     
